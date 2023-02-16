65f051923d6223db5489739105fe5f24c9db642e
RicardoAMirandaJ <124701989+RicardoAMirandaJ@users.noreply.github.com>
2023-02-15 20:04:56 -0600

    Add files via upload

---
+++ EVA1_5_CIRCULO/src/eva1_5_circulo/EVA1_5_CIRCULO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_circulo;
+
+/**
+ *
+ * @author ricar
+ */
+public class EVA1_5_CIRCULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Circulo circ = new Circulo();
+        
+        circ.setRadio(30);
+        circ.setArea(0);
+        circ.setPerimetro(0);
+        circ.imprimirDatos();
+    }
+    
+}

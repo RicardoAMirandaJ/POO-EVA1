5163a87b9cd8cb5f9beddbb02f7dace88d90130a
RicardoAMirandaJ <124701989+RicardoAMirandaJ@users.noreply.github.com>
2023-02-21 09:39:25 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author ricar
+ */
+public class EVA1_7_METODOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myMethod();//invocar al método: llamada a función
+    }
+    public static void myMethod(){
+        System.out.println("You have called me: my name is myMethod!!");
+    }
+}
